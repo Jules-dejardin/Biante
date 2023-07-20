a462ee584ca633f21f7a7ec62f041ba62a310b37
Elie Gavoty <eliegavoty@free.fr>
2023-07-20 18:21:18 +0200

    session 20  juillet

--- test.py
+++ test.py
@@ -53,6 +53,10 @@ rr >> play("[XXX]", dur=.5, sample=PRand(0,2), pan=var([-1,0,1,0],PRand(2,8)), r
 rr.rate=0
 rr.pan=0
 
+jj >> play("[XXXXXXXXXXXXXXX]......", rate=[3,4,1,5,8,8,8]).stop().ampfadeout(famp=.2)
+
+ui >> play("Y.[=].", sample="9").stop()
+
 rr.ampfadeout(8)
 
 jj.ampfadeout()
@@ -63,4 +67,40 @@ a3 >> blip([0], dur=.25, oct=linvar([6,9,7,5,8,4]), pan=linvar([-1,1],PRand(2,16
 
 a3.ampfadeout()
 
-k8 >> play('<b{.b[bb]b[bbbbb]}><[vv]>', rate=linvar([.1,2], 16)).ampfadeout()
+k9 >> play('<b{.b[bb]b[bbbbb]}><[vv]>', rate=linvar([.1,2], 16)).ampfadein(32)#.stop()
+
+k9.stop()
+
+
+k8 >> play('[bb]', rate=.2).fadeout(16)
+
+ii >> blip(amp=)
+
+#######
+
+p2 >> padarp(
+    [0, 2, 7],
+    dur=2/4,
+    oct=[3,7,9,4,5,6],
+    amp=.8,
+    verb=0,
+    delay=0,
+    detune=0,
+    expand=0,
+    # vol=1.2,
+)
+# p2.span(srot(12), .5)
+# p2.fadein(16, fvol=.4)
+p2.detune=sinvar([0,1], PWhite(.2,3)[:16], start=Clock.mod(4)),
+p2.expand=linvar([0, 1], 24, start=Clock.mod(4))
+
+p2.fadeout()
+
+p2.verb=linvar([0,1], [32], start=Clock.mod(4))
+
+p2.delay=linvar([0,1], [64], start=Clock.mod(4))
+
+
+a2 >> apad(0, oct=7, dur=5) + (-6, -12)
+
+a2.oct=3
